51bbd61490627c1929b8c5fb78702580bfdf3ea5
Aerotheory <aerotheory@gmail.com>
2021-11-23 18:29:39 +1000

    first commit

---
+++ futter_starter/android/app/src/main/kotlin/com/example/futter_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.futter_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ futter_starter/lib/main.dart
@@ -0,0 +1,32 @@
+// flutter emulators --launch Pixel 4
+// git branch -m master main
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rewards App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Rewards App'),
+        ),
+        body: const Center(
+          child: Text('hello'),
+        ),
+      ),
+    );
+  }
+}
